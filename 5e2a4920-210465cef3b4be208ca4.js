2e68694af5edc84207d782075a4e272726347dc3
heoyejida <heoyejida@gmail.com>
2022-04-18 00:52:56 +0900

    Updates

---
+++ 5e2a4920-210465cef3b4be208ca4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkheoyejida=self.webpackChunkheoyejida||[]).push([[152],{3750:function(e,n,t){t.d(n,{bTu:function(){return r}});var o=t(1046);function r(e){return(0,o.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"polyline",attr:{points:"6 9 12 15 18 9"}}]})(e)}}}]);
+//# sourceMappingURL=5e2a4920-210465cef3b4be208ca4.js.map
